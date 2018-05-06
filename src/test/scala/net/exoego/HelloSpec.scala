9e41d0ddbe0411d97068ace6842fad529dee3b7b
exoego <mogami@exoego.net>
2018-05-06 21:31:26 +0900

    Scaffold Scala project

---
+++ src/test/scala/net/exoego/HelloSpec.scala
@@ -0,0 +1,15 @@
+package net.exoego
+
+import org.scalatest.{FunSpec, Matchers}
+
+class HelloSpec extends FunSpec with Matchers {
+
+  describe("example hierarchy") {
+
+    it("should work") {
+      1 + 1 shouldBe 2
+    }
+
+  }
+
+}
