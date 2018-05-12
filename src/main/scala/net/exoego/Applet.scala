06cb50a968df6b67a037f2885fbc50f62b2a138e
exoego <mogami@exoego.net>
2018-05-12 19:54:39 +0900

    Refactor: Separate view-concern (color) from Cell

--- src/main/scala/net/exoego/Applet.scala
+++ src/main/scala/net/exoego/Applet.scala
@@ -4,70 +4,12 @@ import processing.core.PApplet
 import processing.core.PApplet._
 import processing.core.PConstants._
 
-import scala.util.Random
-
 class Applet extends PApplet {
   private final val COLOR_ALIVE = color(24, 160, 167)
-  private final val COLOR_DEAD  = color(0)
-  private final val COLOR_GRID  = color(48)
+  private final val COLOR_DEAD = color(0)
+  private final val COLOR_GRID = color(48)
 
-  private final val cellSize = 5
-
-  sealed trait CellState
-
-  case object Dead extends CellState
-
-  case object Alive extends CellState
-
-  object Cell {
-    private final val rand: Random              = new Random(java.security.SecureRandom.getInstanceStrong)
-    private final val probabilityOfAliveAtStart = 15
-  }
-
-  case class Cell(private var state: CellState, private var lastState: CellState) {
-    import Cell._
-
-    def toggle(): Unit = {
-      if (wasActiveLast()) {
-        this.die()
-      } else {
-        this.born()
-      }
-    }
-
-    def color(): Int = {
-      this.state match {
-        case Dead  => COLOR_DEAD
-        case Alive => COLOR_ALIVE
-      }
-    }
-
-    def mutate(): Unit = {
-      if (rand.nextInt(100) > probabilityOfAliveAtStart) {
-        this.die()
-      } else {
-        this.born()
-      }
-    }
-
-    def wasActiveLast(): Boolean = this.lastState == Alive
-
-    def saveState(): Unit = {
-      this.lastState = this.state
-    }
-
-    def nextState(neighbours: Int): Unit = {
-      this.state match {
-        case Alive if neighbours < 2 || neighbours > 3 => die()
-        case Dead if neighbours == 3                   => born()
-        case _                                         => // do nothing
-      }
-    }
-
-    def die(): Unit = this.state = Dead
-
-    def born(): Unit = this.state = Alive
-  }
+  private final val cellSize = 10
 
   private var cells: Array[Array[Cell]] = Array.empty
 
@@ -78,7 +20,7 @@ class Applet extends PApplet {
   private def cols_ = height / cellSize
 
   override def settings(): Unit = {
-    size(720, 720, JAVA2D)
+    size(900, 900, JAVA2D)
     noSmooth()
   }
 
@@ -132,6 +74,13 @@ class Applet extends PApplet {
     }
   }
 
+  private def draw(cell: Cell): Unit = {
+    fill(cell.currentState() match {
+      case Dead => COLOR_DEAD
+      case Alive => COLOR_ALIVE
+    })
+  }
+
   private def toggleCellStateByMouseClick(): Unit = {
     val xCellOver = {
       val cellOver = map(mouseX.toFloat, 0f, width.toFloat, 0f, rows_.toFloat).toInt
@@ -142,15 +91,16 @@ class Applet extends PApplet {
       constrain(cellOver, 0, cols_ - 1)
     }
 
-    cells(xCellOver)(yCellOver).toggle()
-    fill(cells(xCellOver)(yCellOver).color())
+    val cell = cells(xCellOver)(yCellOver)
+    cell.toggle()
+    draw(cell)
   }
 
   private def drawCell() = {
     for {
       (x, y) <- coordinates()
     } {
-      fill(cells(x)(y).color())
+      draw(cells(x)(y))
       rect((x * cellSize).toFloat, (y * cellSize).toFloat, cellSize, cellSize)
     }
   }
@@ -177,18 +127,18 @@ class Applet extends PApplet {
   final val bounded: (Int, Int) => Seq[Int] = (current: Int, rangeMax: Int) => {
     val limit = rangeMax - 1
     current match {
-      case 0       => Array(current, current + 1)
+      case 0 => Array(current, current + 1)
       case `limit` => Array(current - 1, current)
-      case _       => Array(current - 1, current, current + 1)
+      case _ => Array(current - 1, current, current + 1)
     }
   }
 
   final val troidal: (Int, Int) => Seq[Int] = (current: Int, rangeMax: Int) => {
     val limit = rangeMax - 1
     current match {
-      case 0       => Array(rangeMax - 1, current, current + 1)
+      case 0 => Array(rangeMax - 1, current, current + 1)
       case `limit` => Array(current - 1, current, 0)
-      case _       => Array(current - 1, current, current + 1)
+      case _ => Array(current - 1, current, current + 1)
     }
   }
 
@@ -219,9 +169,9 @@ class Applet extends PApplet {
   override def keyPressed(): Unit = {
     key match {
       case 'r' | 'R' => initializeCells()
-      case ' '       => togglePause()
+      case ' ' => togglePause()
       case 'c' | 'C' => clearAllCells()
-      case _         => // do nothing
+      case _ => // do nothing
     }
   }
 
--- src/main/scala/net/exoego/Cell.scala
+++ src/main/scala/net/exoego/Cell.scala
@@ -1,3 +1,55 @@
 package net.exoego
 
-class Cell {}
+import scala.util.Random
+
+sealed trait CellState
+
+case object Dead extends CellState
+
+case object Alive extends CellState
+
+object Cell {
+  private final val rand: Random              = new Random(java.security.SecureRandom.getInstanceStrong)
+  private final val probabilityOfAliveAtStart = 15
+}
+
+case class Cell(private var state: CellState, private var lastState: CellState) {
+
+  def currentState(): CellState = this.state
+
+  import Cell._
+
+  def toggle(): Unit = {
+    if (wasActiveLast()) {
+      this.die()
+    } else {
+      this.born()
+    }
+  }
+
+  def mutate(): Unit = {
+    if (rand.nextInt(100) > probabilityOfAliveAtStart) {
+      this.die()
+    } else {
+      this.born()
+    }
+  }
+
+  def wasActiveLast(): Boolean = this.lastState == Alive
+
+  def saveState(): Unit = {
+    this.lastState = this.state
+  }
+
+  def nextState(neighbours: Int): Unit = {
+    this.state match {
+      case Alive if neighbours < 2 || neighbours > 3 => die()
+      case Dead if neighbours == 3                   => born()
+      case _                                         => // do nothing
+    }
+  }
+
+  def die(): Unit = this.state = Dead
+
+  def born(): Unit = this.state = Alive
+}
