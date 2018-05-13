c8fe2158fdaec96f73e60ee4dc935af4c4655942
exoego <mogami@exoego.net>
2018-05-14 08:34:47 +0900

    Refactor: Move class to its own file

---
+++ src/main/scala/net/exoego/BoundaryProcessor.scala
@@ -0,0 +1,27 @@
+package net.exoego
+
+sealed trait BoundaryProcessor {
+  def apply(current: Int, rangeMax: Int): Seq[Int]
+}
+
+case object Bounded extends BoundaryProcessor {
+  override def apply(current: Int, rangeMax: Int): Seq[Int] = {
+    val limit = rangeMax - 1
+    current match {
+      case 0       => Array(current, current + 1)
+      case `limit` => Array(current - 1, current)
+      case _       => Array(current - 1, current, current + 1)
+    }
+  }
+}
+
+case object Loop extends BoundaryProcessor {
+  override def apply(current: Int, rangeMax: Int): Seq[Int] = {
+    val limit = rangeMax - 1
+    current match {
+      case 0       => Array(rangeMax - 1, current, current + 1)
+      case `limit` => Array(current - 1, current, 0)
+      case _       => Array(current - 1, current, current + 1)
+    }
+  }
+}
